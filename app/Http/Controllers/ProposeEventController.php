104d9f038b96fa714b592253dcd30f4a64fac466
paduCosty <padu.costi@yahoo.com>
2023-05-04 14:21:26 +0300

    fix

--- app/Http/Controllers/ProposeEventController.php
+++ app/Http/Controllers/ProposeEventController.php
@@ -12,7 +12,7 @@ class ProposeEventController extends Controller
 {
     public function index(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $eventLocations = UserEventLocation::all();
+        $eventLocations = UserEventLocation::paginate(10);
         return view('admin.propose-event.index', compact('eventLocations',));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,7 @@ require __DIR__ . '/auth.php';
 //Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
