212129b143e556550e08fec5ba116e5f36340bd1
paduCosty <78753049+paduCosty@users.noreply.github.com>
2023-05-11 14:19:17 +0300

    Revert "design home-page-cards"

--- resources/views/components/modals/terms_and_conditions_modal.blade.php
+++ resources/views/components/modals/terms_and_conditions_modal.blade.php
@@ -10,7 +10,7 @@
                         <div class="modal-close-text-container">
                             <button type="button" class="close-modal-button" data-bs-dismiss="modal"
                                     aria-label="Close"
-                                    id="close-modal-9"> <span style="color: #a00404">X</span></button>
+                                    id="close-modal-9">Închide <span style="color: #A6CE39">[X]</span></button>
                         </div>
                     </div>
 
