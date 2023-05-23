3d4c7fc31bb9803732cbce5e314748bc837af766
paduCosty <padu.costi@yahoo.com>
2023-05-23 16:04:14 +0300

    fix

--- resources/views/components/modals/enrol_modal.blade.php
+++ resources/views/components/modals/enrol_modal.blade.php
@@ -106,7 +106,8 @@
                                         <div class="col-12 col-sm-7">
                                             <div class="row">
                                                 <div class="col-12">
-                                                    <select name="city" required id="region_cities_enrol">
+                                                    <select name="city" class="form-control fs-6" required
+                                                            id="region_cities_enrol">
                                                         <option value="">selecteaza</option>
                                                     </select>
                                                 </div>
@@ -114,141 +115,137 @@
                                         </div>
                                     </div>
 
-
                                     <div class="row form-group">
                                         <div class="col-12 col-sm-5">
                                             <label for="friends_enrolled_modal_2_general"
-                                                   class="fs-6 col-form-label form-modal-label">Mai vin</label>
+                                                   class="fs-6 col-form-label form-modal-label">Cati prieteni mai vin cu
+                                                tine?</label>
 
+                                        </div>
+                                        <div class="col-12 col-sm-5">
+                                            <div class="col-12 col-sm-6">
+                                                <input type="text" required
+                                                       class="form-control fs-6 input-number-of-friends-eco4 eco-input-text-modal"
+                                                       name="transport" placeholder="Numar"
+                                                       id="friends_enrolled_modal_2_general"/>
+                                                {{--  <label class="fs-6"></label>  --}}
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row form-group" id="i-have-car-section">
+                                        <div class="col-12 col-sm-5 fs-6" style="color:gray">
+                                            <label for="car_modal_2_general">Am mașină și am </label>
                                         </div>
 
                                         <div class="col-12 col-sm-5">
+                                            <div class="fix-alignment-1">
+                                                <div>
+                                                    <input type="text" placeholder="Locuri" required
+                                                           class=" form-control input-number-of-seats-eco4"
+                                                           name="seats_available" id="car_modal_2_general"/>
+                                                </div>
 
-
-                                            <div class="col-12 col-sm-6">
-                                                <input type="text" required
-                                                       class=" fs-6 form-control input-number-of-friends-eco4 eco-input-text-modal"
-                                                       name="transport" placeholder="Numar"
-                                                       id="friends_enrolled_modal_2_general"
-                                                />prieteni să facem
-                                                treabă
-                                                <label
-                                                    class="fs-6"></label>
-
+                                                <div class="fs-6">
+                                                    <label for="car_modal_2_general" style="color:gray"> locuri
+                                                        libere</label>
+                                                </div>
                                             </div>
+                                        </div>
+                                        <div class="row form-group">
+                                            <div class="col-12 text-start " id="field-completion-message">
+                                                <label id="confirmation-for-number-of-people"
+                                                       style="color:rgb(173, 172, 172); font-size:13px;">
+                                                    Atenție: completarea acestui câmp presupune că vei
+                                                    lua de la locul de pornire numărul de persoane specificat
+                                                </label>
+                                            </div>
+                                        </div>
 
-                                            {{-- <div class="col-12 col-sm-6">
-                                                <label for="friends_enrolled_modal_2_general"
-                                                       class=" form-control fs-6 friends-enrolled"
-                                                       style="width: 133px; height: 17px;"></label>
-                                            </div> --}}
+                                        <div class="row form-group">
+                                            <div class="col-12 text-start" id="field-completion-message">
+                                                <label id="confirmation-for-number-of-people">
+                                                    Atenție: completarea acestui câmp presupune că vei
+                                                    lua de la locul de pornire numărul de persoane specificat
+                                                </label>
+                                            </div>
+                                        </div>
 
+                                        <div class="row form-group m-1">
+                                            <div class="col-12">
+                                                <div class="terms-conditions-text">
+                                                    <span id="eco-action-general-enroll-modal-check-1-span"></span>
+                                                    <input class="form-check-input" required value="1" type="checkbox"
+                                                           name="terms_site"
+                                                           id="eco-action-general-enroll-modal-check-1"/>
+                                                    <label style="display: inline;" style="display: inline;"
+                                                           for="eco-action-general-enroll-modal-check-1">Sunt de
+                                                        acord
+                                                        cu </label>
+                                                    <a href="#" data-bs-toggle="modal" data-bs-target="#tandc"
+                                                       style="color: #A6CE39;">termenii și condițiile acestui</a>
+                                                    site.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row form-group m-1">
+                                            <div class="col-12">
+                                                <div class="terms-conditions-text">
+                                                    <span id="eco-action-general-enroll-modal-check-2-span"></span>
+                                                    <input class="form-check-input" required value="1" type="checkbox"
+                                                           name="terms_workshop"
+                                                           id="eco-action-general-enroll-modal-check-2"/>
+                                                    <label style="display: inline;" style="display: inline;"
+                                                           for="eco-action-general-enroll-modal-check-2">Sunt de
+                                                        acord
+                                                        cu </label>
+                                                    <a href="#" data-bs-toggle="modal" data-bs-target="#workshop"
+                                                       style="color: #A6CE39;">termenii și condițiile de
+                                                        participare</a>
+                                                    la
+                                                    workshop-ul de ecologizare.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row form-group m-1">
+                                            <div class="col-12">
+                                                <div class="terms-conditions-text">
+                                                    <span id="eco-action-general-enroll-modal-check-3-span"></span>
+                                                    <input class="form-check-input" required value="1" type="checkbox"
+                                                           name="volunteering_contract"
+                                                           id="eco-action-general-enroll-modal-check-3"/>
+                                                    <label style="display: inline;" style="display: inline;"
+                                                           for="eco-action-general-enroll-modal-check-3">Sunt de
+                                                        acord
+                                                        cu </label>
+                                                    <a href="#" data-bs-toggle="modal" data-bs-target="#contract"
+                                                       style="color: #A6CE39;;">contractul de voluntariat.</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <input type="hidden" required class="users_event_location_id"
+                                               name="users_event_location_id">
+                                        <div class="error-messages"></div>
+
+                                        <div class="row form-group">
+                                            <div class="col-12 fix-alignment-2">
+                                                <input type="submit" id="volunteer-enroll-general-button"
+                                                       class="form-submit modal-register-button"
+                                                       value="Mă inscriu"/>
+                                            </div>
                                         </div>
                                     </div>
+                                </form>
                             </div>
-
-                            <div class="row form-group" id="i-have-car-section">
-                                <div class="col-12 col-sm-5">
-                                    <label for="car_modal_2_general">Am mașină și</label>
-                                </div>
-
-                                <div class="col-12 col-sm-7">
-                                    <div class="fix-alignment-1">
-                                        <div>
-                                            <input type="text" required class="input-number-of-seats-eco4"
-                                                   name="seats_available" id="car_modal_2_general"/>
-                                        </div>
-
-                                        <div>
-                                            <label for="car_modal_2_general" style="padding-right: 15px;">locuri
-                                                libere in ea</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row form-group">
-                                    <div class="col-12 text-start" id="field-completion-message">
-                                        <label id="confirmation-for-number-of-people">
-                                            Atenție: completarea acestui câmp presupune că vei
-                                            lua de la locul de pornire numărul de persoane specificat
-                                        </label>
-                                    </div>
-                                </div>
-
-                                <div class="row form-group m-1">
-                                    <div class="col-12">
-                                        <div class="terms-conditions-text">
-                                            <span id="eco-action-general-enroll-modal-check-1-span"></span>
-                                            <input class="form-check-input" required value="1" type="checkbox"
-                                                   name="terms_site"
-                                                   id="eco-action-general-enroll-modal-check-1"/>
-                                            <label style="display: inline;" style="display: inline;"
-                                                   for="eco-action-general-enroll-modal-check-1">Sunt de
-                                                acord
-                                                cu </label>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#tandc"
-                                               style="color: #A6CE39;">termenii și condițiile acestui</a>
-                                            site.
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row form-group m-1">
-                                    <div class="col-12">
-                                        <div class="terms-conditions-text">
-                                            <span id="eco-action-general-enroll-modal-check-2-span"></span>
-                                            <input class="form-check-input" required value="1" type="checkbox"
-                                                   name="terms_workshop"
-                                                   id="eco-action-general-enroll-modal-check-2"/>
-                                            <label style="display: inline;" style="display: inline;"
-                                                   for="eco-action-general-enroll-modal-check-2">Sunt de
-                                                acord
-                                                cu </label>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#workshop"
-                                               style="color: #A6CE39;">termenii și condițiile de
-                                                participare</a>
-                                            la
-                                            workshop-ul de ecologizare.
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row form-group m-1">
-                                    <div class="col-12">
-                                        <div class="terms-conditions-text">
-                                            <span id="eco-action-general-enroll-modal-check-3-span"></span>
-                                            <input class="form-check-input" required value="1" type="checkbox"
-                                                   name="volunteering_contract"
-                                                   id="eco-action-general-enroll-modal-check-3"/>
-                                            <label style="display: inline;" style="display: inline;"
-                                                   for="eco-action-general-enroll-modal-check-3">Sunt de
-                                                acord
-                                                cu </label>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#contract"
-                                               style="color: #A6CE39;;">contractul de voluntariat.</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <input type="hidden" required class="users_event_location_id"
-                                       name="users_event_location_id">
-                                <div class="error-messages"></div>
-
-                                <div class="row form-group">
-                                    <div class="col-12 fix-alignment-2">
-                                        <input type="submit" id="volunteer-enroll-general-button"
-                                               class="form-submit modal-register-button"
-                                               value="Mă inscriu"/>
-                                    </div>
-                                </div>
-                            </div>
-                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 
 <script>
