408fb865856da7d47f1101862312f470994dfa8f
Bianca <biabiancaalbertina@yahoo.com>
2023-05-25 11:37:15 +0300

    black-terms-conditions

--- resources/views/components/modals/propose-event-location-modal.blade.php
+++ resources/views/components/modals/propose-event-location-modal.blade.php
@@ -195,10 +195,10 @@
                                             </div>
 
                                             <div class="row m-1">
-                                                <div class="col-12 terms-conditions-text text-dark fs-6">
+                                                <div class="col-12 terms-conditions-text  fs-6" style="color:rgb(150, 149, 149)">
                                                     <span id="eco-proposal-modal-check-1-span"></span>
                                                     <input type="checkbox" name="terms_site" value="1" required/>
-                                                    <label class="text-dark fs-6" for="eco-proposal-modal-check-1">Sunt
+                                                    <label class="fs-6" for="eco-proposal-modal-check-1">Sunt
                                                         de acord cu</label>
                                                     <a href="#" data-bs-toggle="modal" data-bs-target="#tandc"
                                                        style="color: #A6CE39;">Termenii
@@ -208,7 +208,7 @@
                                             </div>
 
                                             <div class="row m-1">
-                                                <div class="col-12 terms-conditions-text text-dark fs-6">
+                                                <div class="col-12 terms-conditions-text fs-6" style="color:rgb(150, 149, 149)">
                                                     <input type="checkbox" name="terms_workshop"
                                                            id="eco-proposal-modal-check-2" value="1" required/>
                                                     <label style="display: inline;"
@@ -223,7 +223,7 @@
                                             </div>
 
                                             <div class="row m-1">
-                                                <div class="col-12 terms-conditions-text text-dark fs-6">
+                                                <div class="col-12 terms-conditions-text  fs-6" style="color:rgb(150, 149, 149)">
                                                     <span id="eco-proposal-modal-check-3-span"></span>
                                                     <input type="checkbox" value="1"
                                                            name="volunteering_contract"
